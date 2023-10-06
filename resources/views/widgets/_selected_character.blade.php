4be0fd1c4c7ababe63f7e77689c4c457b74c45c9
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-06 19:54:25 -0400

    no message

--- resources/views/widgets/_selected_character.blade.php
+++ resources/views/widgets/_selected_character.blade.php
@@ -4,7 +4,7 @@
         <div class="profile-assets-content">
             @if($character)
                 <div>
-                    <a href="{{ $character->url }}"><img src="{{ isset($fullImage) && $fullImage ? $character->image->imageUrl : $character->image->thumbnailUrl }}" class="{{ isset($fullImage) && $fullImage ? '' : 'img-thumbnail' }}" alt="{{ $character->fullName }}" /></a>
+                    <a href="{{ $character->url }}"><img class="img-fluid" src="{{ isset($fullImage) && $fullImage ? $character->image->imageUrl : $character->image->thumbnailUrl }}" class="{{ isset($fullImage) && $fullImage ? '' : 'img-thumbnail' }}" alt="{{ $character->fullName }}" /></a>
                 </div>
                 <div class="my-1">
                     <a href="{{ $character->url }}" class="h5 mb-0"> @if(!$character->is_visible) <i class="fas fa-eye-slash"></i> @endif {{ $character->fullName }}</a>
