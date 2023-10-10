29342aa02228280ba562811d880071cac5ad927b
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-10 14:53:07 -0400

    no message

--- resources/views/inventory/_box.blade.php
+++ resources/views/inventory/_box.blade.php
@@ -1,8 +1,8 @@
 <li class="list-group-item">
-    <a class="card-title h5 collapse-title"  data-toggle="collapse" href="#openBoxForm"> Open Box</a>
+    <a class="card-title h5 collapse-title"  data-toggle="collapse" href="#openBoxForm"> Open Item</a>
     <div id="openBoxForm" class="collapse">
         {!! Form::hidden('tag', $tag->tag) !!}
-        <p>This action is not reversible. Are you sure you want to open this box?</p>
+        <p>This action is not reversible. Are you sure you want to open this item?</p>
         <div class="text-right">
             {!! Form::button('Open', ['class' => 'btn btn-primary', 'name' => 'action', 'value' => 'act', 'type' => 'submit']) !!}
         </div>
