5ce284441e5e17edc5652358c4c8e6d1304b3829
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-15 19:30:28 -0400

    no message

--- resources/views/layouts/_terms_modal.blade.php
+++ resources/views/layouts/_terms_modal.blade.php
@@ -7,11 +7,11 @@
             <div class="modal-header" style="border-top: 2px dashed #e5c1c7;">
                 <h5 class="modal-title" style="color: #95b582;">{{ Config::get('lorekeeper.settings.terms_popup')['title'] }}</h5>
             </div>
-            <div class="modal-body">
+            <div class="modal-body" style="margin-bottom: -30px;">
             <p>You must be 18+ to access this site.<br />By clicking 'Accept', you are confirming to be 18 years or older and to follow the <a href="{{ url('info/terms') }}">Terms of Service</a>.</p>
             </div>
             
-            <div class="modal-footer" style="border-top: 0px; border-bottom: 2px dashed #e5c1c7; margin-top: -10px;">
+            <div class="modal-footer" style="border-top: 0px; border-bottom: 2px dashed #e5c1c7;">
                 <button type="button" class="btn btn-primary" id="termsButton">               
                     {{ Config::get('lorekeeper.settings.terms_popup')['button'] }}
                 </button>
