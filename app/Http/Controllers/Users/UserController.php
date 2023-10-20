583e6cf2f82168303591c5a5d6b427c2d290e6a6
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-19 20:12:02 -0400

    no message

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -103,7 +103,6 @@ class UserController extends Controller
             'items' => $this->user->items()->where('count', '>', 0)->orderBy('user_items.updated_at', 'DESC')->take(4)->get(),
             'awards' => $this->user->awards()->orderBy('user_awards.updated_at', 'DESC')->whereNull('deleted_at')->where('count','>',0)->take(4)->get(),
             'pets' => $this->user->pets()->orderBy('user_pets.updated_at', 'DESC')->take(5)->get(),
-            'sublists' => Sublist::orderBy('sort', 'DESC')->get(),
             'characters' => $characters,
             'armours' => $armours,
             'user_enabled' => Settings::get('WE_user_locations'),
