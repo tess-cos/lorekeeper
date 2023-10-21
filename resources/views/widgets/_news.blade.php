b5577c90a4ee4e9dbccf242e7cec87810fa3e253
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-21 12:03:16 -0400

    no message

--- resources/views/widgets/_news.blade.php
+++ resources/views/widgets/_news.blade.php
@@ -1,5 +1,5 @@
 <div class="card mb-4" style="margin-top: 10px; border: 0px;">
-    <div class="card-header" style="background-color: none; border: 0px;">
+    <div class="card-header" style="background-color: #fff !important; border: 0px;">
         <h4 class="mb-0" style="color: #95b582;">✿ Recent News</h4>
     </div>
 
@@ -9,7 +9,7 @@
                 <div style="padding: 5px; padding-top: 15px; background-color: #fafafa; color: #6a6a6a; border-bottom: 10px solid #fff;">
                     <span class="d-flex flex-column flex-sm-row align-items-sm-end @if(!$textPreview) pb-3 @endif" style="border: 0px; padding: 2.5px;">
                         <h5 class="mb-0" style="padding-left: 13px; font-family: Poppins, serif; font-size: 11.5pt;"> {!! $news->displayName !!}</h5>
-                        <span class="ml-2 small">Posted {!! $news->post_at ? pretty_date($news->post_at) : pretty_date($news->created_at) !!}</span>
+                        <span class="ml-2 small" style="margin-bottom: -2px;">Posted {!! $news->post_at ? pretty_date($news->post_at) : pretty_date($news->created_at) !!}</span>
                     </span>
                     @if($textPreview)
                         <p class="pl-3">{!! substr(strip_tags(str_replace("<br />", "&nbsp;", $news->parsed_text)), 0, 85) !!}... <a style="color: #95b582;; font-weight: bold; font-family: Poppins, serif;" href="{!! $news->url !!}">read more <i class="fas fa-arrow-right"></i></a></p>
