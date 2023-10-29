6c510094ac22d46a7e46a588b35a354995de3d62
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-10-28 22:32:53 -0400

    no message

--- resources/views/dailies/_sidebar.blade.php
+++ resources/views/dailies/_sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul><div class="sideimg"></div>
     <li class="sidebar-header"><a href="{{ url(__('dailies.dailies')) }}" class="card-link">{{__('dailies.dailies')}}</a></li>
 
-    <li class="sidebar-section">
+    <li class="sidebar-section" style="border-bottom-left-radius: .50rem; border-bottom-right-radius: .50rem; padding-bottom: 10px;">
         @foreach($dailies as $daily)
         <div class="sidebar-item"><a href="{{ $daily->url }}" class="{{ set_active('dailies/'.$daily->id) }}">{{ $daily->name }}</a></div>
         @endforeach
