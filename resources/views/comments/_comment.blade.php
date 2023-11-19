a8de09b1ecd9e18ed2497d353fb5d8180586134b
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2023-11-19 10:13:23 -0500

    forgor featured comment css

--- resources/views/comments/_comment.blade.php
+++ resources/views/comments/_comment.blade.php
@@ -21,7 +21,7 @@
                 </h5>
                 @if($comment->is_featured)<div class="ml-1 text-muted text-right col-6 mx-0 pr-1"><small class="text-success">Featured by Owner</small></div> @endif
             </div>
-            <div class="p-3 rounded {{ $comment->is_featured ? 'border-success bg-light' : '' }} " style="border: 1px solid #E0ECD0;"><p style="padding: 5px;">{!! nl2br($markdown->line($comment->comment)) !!} </p>
+            <div class="p-3 rounded {{ $comment->is_featured ? 'cmtf' : '' }} " style="border: 1px solid #E0ECD0;"><p style="padding: 5px;">{!! nl2br($markdown->line($comment->comment)) !!} </p>
             <p class="pt-1 text-right mb-0">
                 <small class="text-muted">{!! $comment->created_at !!}
                 @if($comment->created_at != $comment->updated_at) 
