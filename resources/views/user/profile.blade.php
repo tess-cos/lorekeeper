6163e4502b6d5a5125b3fe173cb9b21d0ae820a0
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-01-05 00:29:39 -0500

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -83,7 +83,7 @@
 
 @if(isset($user->profile->parsed_text))
     <div class="card mb-3" style="clear:both; border: 0px; padding: 0px;">
-        <div class="card-body" style="background: transparent !important;">
+        <div class="card-body" style="background: #fcfcfc !important;">
             {!! $user->profile->parsed_text !!}
         </div>
     </div>
