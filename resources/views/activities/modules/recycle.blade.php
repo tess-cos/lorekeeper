f133c458ed3058be4331b12390e8c342ceed1c8f
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-02-01 09:05:44 -0500

    activites fix + small user shop credit edit

--- resources/views/activities/modules/recycle.blade.php
+++ resources/views/activities/modules/recycle.blade.php
@@ -3,11 +3,11 @@
 @if($activity->data->quantity)
 <p>You are required to select {{ $activity->data->quantity }} item(s) to turn in.</p>
 @endif
-@include('widgets._inventory_select', ['user' => Auth::user(), 'inventory' => $inventory, 'categories' => $categories, 'selected' => [], 'hideCollapse' => true])
+@include('widgets._inventory_select_sc', ['user' => Auth::user(), 'inventory' => $inventory, 'categories' => $categories, 'selected' => [], 'hideCollapse' => true])
 <div class="text-right">
     {!! Form::submit('Turn in Items', ['class' => 'btn btn-primary']) !!}
 </div>
 
 {!! Form::close() !!}
 
-@include('widgets._inventory_select_js')
+@include('widgets._inventory_select_sc_js')
