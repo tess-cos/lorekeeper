44ba806c1f692869fa24223447d4354aa9782a18
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-02-01 23:12:45 -0500

    collapsed activity invent select

--- resources/views/activities/modules/recycle.blade.php
+++ resources/views/activities/modules/recycle.blade.php
@@ -3,10 +3,13 @@
 @if($activity->data->quantity)
 <p>You are required to select {{ $activity->data->quantity }} item(s) to turn in.</p>
 @endif
+<h3>Inventory <a class="small inventory-collapse-toggle collapse-toggle" href="#inventturnin" data-toggle="collapse">Show</a></h3>
+<hr>
+    <div class="collapse collapse" id="inventturnin">
 @include('widgets._inventory_select_sc', ['user' => Auth::user(), 'inventory' => $inventory, 'categories' => $categories, 'selected' => [], 'hideCollapse' => true])
 <div class="text-right">
     {!! Form::submit('Turn in Items', ['class' => 'btn btn-primary']) !!}
-</div>
+</div></div>
 
 {!! Form::close() !!}
 
