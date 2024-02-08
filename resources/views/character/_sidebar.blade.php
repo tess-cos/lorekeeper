05cf3d4ff29c0900221dbbba671d2d9ad914d3cd
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-02-07 19:35:06 -0500

    remove plural from character drop sidebar

--- resources/views/character/_sidebar.blade.php
+++ resources/views/character/_sidebar.blade.php
@@ -9,7 +9,7 @@
         <div class="sidebar-item"><a href="{{ $character->url . '/bank' }}" class="{{ set_active('character/'.$character->slug.'/bank') }}">Bank</a></div>
         <div class="sidebar-item"><a href="{{ $character->url . '/'.__('awards.awardcase') }}" class="{{ set_active('character/'.$character->slug.'/'.__('awards.awardcase')) }}">{{ucfirst(__('awards.awards'))}}</a></div>
         @if($character->image->species->hasDrops && ($character->drops->dropData->isActive || (Auth::check() && Auth::user()->hasPower('manage_characters'))))
-    <div class="sidebar-item"><a href="{{ $character->url . '/drops' }}" class="{{ set_active('character/'.$character->slug.'/drops') }}">Collect {{ isset($character->drops->dropData->data['drop_name']) ? $character->drops->dropData->data['drop_name'].'s' : 'Drops' }}</a></div>
+    <div class="sidebar-item"><a href="{{ $character->url . '/drops' }}" class="{{ set_active('character/'.$character->slug.'/drops') }}">Collect {{ isset($character->drops->dropData->data['drop_name']) ? $character->drops->dropData->data['drop_name'].'' : 'Drops' }}</a></div>
         @endif
     </li>
     <li class="sidebar-section" style="border-bottom-left-radius: .50rem; border-bottom-right-radius: .50rem; padding-bottom: 10px;">
