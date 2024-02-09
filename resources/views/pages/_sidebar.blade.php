a2c4457c9c0d429613070d50219767e4f3bf21ff
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-02-09 10:23:30 -0500

    minor css tweaks

--- resources/views/pages/_sidebar.blade.php
+++ resources/views/pages/_sidebar.blade.php
@@ -8,7 +8,9 @@
 </li>
 
 
-            <li class="sidebar-header-fc" style="padding-top: 0.5px;"> <a style="color: #95b582; font-family: Mali; font-size: 12pt;" href="{{ $featured->url }}" class="h5 mb-0">@if(!$featured->is_visible) <i class="fas fa-eye-slash"></i> @endif {{ $featured->fullName }}</a>
+            <li class="sidebar-header-fc" style="padding-top: 0.5px;"> <a style="color: #95b582; font-family: Mali; font-size: 12pt;" href="{{ $featured->url }}" class="h5 mb-0">@if(!$featured->is_visible) <i class="fas fa-eye-slash"></i> @endif 
+
+✿ {{ $featured->fullName }}</a>
             </li>
 <li class="sidebar-header-fc p-1" style="border-bottom-left-radius: .50rem; border-bottom-right-radius: .50rem; padding-bottom: 10px;">
             <div style="text-align: center; font-size: 10pt; text-transform: lowercase; padding-bottom: 15px;">
