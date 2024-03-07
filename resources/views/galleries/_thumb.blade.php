ba63043c71ad33ab3d8c6b0c3889cb8219cd3ac2
DESKTOP-TJ60S6A\lolki <lolkirby58@hotmail.com>
2024-03-07 16:08:27 -0500

    gallery thumbs css tweaks

--- resources/views/galleries/_thumb.blade.php
+++ resources/views/galleries/_thumb.blade.php
@@ -15,11 +15,11 @@
                     @endif
                     by {!! $submission->credits !!}
                         @if(isset($gallery) && !$gallery)
-                            <br/>
+                           
                         @else
-                            ・
+                           
                         @endif
-                    {{ $submission->favorites->count() }} {!! Form::button('<i class="fas fa-star"></i> ', ['style' => 'border:0; border-radius:.5em;', 'class' => ($submission->favorites->where('user_id', Auth::user()->id)->first() != null ? 'btn-success' : ''), 'data-toggle' => 'tooltip', 'title' => ($submission->favorites->where('user_id', Auth::user()->id)->first() == null ? 'Add to' : 'Remove from').' your Favorites', 'type' => 'submit']) !!} ・ {{ App\Models\Comment::where('commentable_type', 'App\Models\Gallery\GallerySubmission')->where('commentable_id', $submission->id)->where('type', 'User-User')->count() }} <i class="fas fa-comment"></i>
+                        <br /> {{ $submission->favorites->count() }} {!! Form::button('<i class="fas fa-star"></i> ', ['style' => 'border:0; border-radius:.5em; background: none;', 'class' => ($submission->favorites->where('user_id', Auth::user()->id)->first() != null ? 'btn-success' : ''), 'data-toggle' => 'tooltip', 'title' => ($submission->favorites->where('user_id', Auth::user()->id)->first() == null ? 'Add to' : 'Remove from').' your Favorites', 'type' => 'submit']) !!} ・ {{ App\Models\Comment::where('commentable_type', 'App\Models\Gallery\GallerySubmission')->where('commentable_id', $submission->id)->where('type', 'User-User')->count() }} <i class="fas fa-comment"></i>
                 {!! Form::close() !!}
             @else
                 @if(isset($gallery) && !$gallery)
@@ -27,7 +27,7 @@
                 @endif
                 by {!! $submission->credits !!}
                     @if(isset($gallery) && !$gallery)
-                        <br/>
+                        
                     @else
                     @endif
                 <br />{{ $submission->favorites->count() }} <i class="fas fa-star" data-toggle="tooltip" title="Favorites"></i> ・ {{ App\Models\Comment::where('commentable_type', 'App\Models\Gallery\GallerySubmission')->where('commentable_id', $submission->id)->where('type', 'User-User')->count() }} <i class="fas fa-comment" data-toggle="tooltip" title="Comments"></i>
